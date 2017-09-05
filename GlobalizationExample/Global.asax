1dc51eb64baff82839678149dacebe545a27866e
Tejashree <tejashree.dhavalikar@gmail.com>
2017-09-05 15:20:55 +0530

    Globalization Assignment

---
+++ GlobalizationExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GlobalizationExample.MvcApplication" Language="C#" %>
---
+++ GlobalizationExample/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using GlobalizationExample.LocalResource;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+using System.Xml.Linq;
+
+namespace GlobalizationExample.Models
+{
+    public class User
+    {
+       
+        [Display(Name = "Name", ResourceType = typeof(Resource))]
+        [Required]
+
+        public string Name { get; set; }
+
+        
+        [Display(Name = "Password", ResourceType = typeof(Resource))]
+        [Required]
+        public string Password { get; set; }
+    }
+}
