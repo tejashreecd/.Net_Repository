09898e747f4ffd03ed22cc3783fa231f1116d334
Tejashree <tejashree.dhavalikar@gmail.com>
2017-09-05 15:49:43 +0530

    Exception Handling Assignment

---
+++ ExceptionExamples/DemoException/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exception_Handling;
+
+namespace DemoException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathLib obj = new MathLib();
+            Console.WriteLine("1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division");
+
+            ///<Generic_exception>
+            //obj.test<ArgumentException>();
+
+
+            Console.WriteLine("Enter your choice: ");
+            int opt = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numbers:");
+
+            try
+            {
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+                switch (opt)
+                {
+                    case 1:
+                        int result = obj.add(num1, num2);
+                        Console.WriteLine("{0}", result);
+                        break;
+                    case 2:
+                        int sub = obj.subtract(num1, num2);
+                        Console.WriteLine("{0}", sub);
+                        break;
+                    case 3:
+                        int mul = obj.multiply(num1, num2);
+                        Console.WriteLine("{0}", mul);
+                        break;
+                    case 4:
+                        int divide = obj.divide(num1, num2);
+                        Console.WriteLine("{0}", divide);
+                        break;
+                }
+
+
+
+
+            }
+
+            catch (FormatException f)
+            {
+                Console.WriteLine("{0}", f.Message);
+            }
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ ExceptionExamples/Exception Handling/MathLib.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        { }
+    }
+    public class MathLib
+    {
+        int result;
+        //bool flag = true;
+        public int add(int num1, int num2)
+        {
+            result = num1 + num2;
+            return result;
+        }
+        public int subtract(int num1, int num2)
+        {
+            result = num1 - num2;
+            return result;
+        }
+        public int multiply(int num1, int num2)
+        {
+            result = num1 * num2;
+            return result;
+        }
+        public int divide(int num1, int num2) 
+        {
+            try
+            {
+                result = num1 / num2;
+                if (num1 < num2)
+                {
+                    throw (new CustomException("Number 1 is greater than number2"));
+                }
+                //IConvertible conv = flag;
+                //Char ch = conv.ToChar(null);
+                //string str = null;
+                //Console.WriteLine("Length: {0}", str.Length);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("{0}", e.Message);
+            }
+            catch (InvalidCastException i)
+            {
+                Console.WriteLine("{0}", i.Message);
+            }
+            catch (NullReferenceException n)
+            {
+                Console.WriteLine("{0}", n.Message);
+            }
+            catch (CustomException c)
+            {
+                Console.WriteLine("{0}", c.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0}", e.Message);
+            }
+            return result;
+        }
+        public void test<T>() where T : Exception, new()
+        {
+            try
+            {
+                throw new T();
+            }
+            catch (T e)
+            {
+                Console.WriteLine("Caught passed in exception type: {0}", e.Message);
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("Caught general exception");
+            }
+        }
+    }
+}
